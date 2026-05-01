f98095013a126848680116eaad397b2e51bd8f0b
Janmarl Paul B. Rodriguez <janmarlbomedianorodriguez@gmail.com>
2026-05-01 09:01:18 +0800

    fix(admin): correct redirect path after giving item to avoid 404

--- src/server-emulator/hiperesp/server/controllers/web/AdminController.php
+++ src/server-emulator/hiperesp/server/controllers/web/AdminController.php
@@ -602,7 +602,7 @@ class AdminController extends Controller {
         }
 
         \http_response_code(302);
-        \header("Location: ../../user?id={$userId}");
+        \header("Location: ../user?id={$userId}");
         return '';
     }
 
